5eb492e533da0fdb606659b3f7b25706c9388a65
“momojihwan” <momojihwan@gmail.com>
2023-10-27 14:52:45 +0900

    fix the scripts

--- egs/librispeech/ASR/scripts/run_960h.sh
+++ egs/librispeech/ASR/scripts/run_960h.sh
@@ -8,8 +8,11 @@
     --decoder-dim 512 --joiner-dim 640 \
 
 # 실험내용 : Dynamic chunk training
+# training option중 --dynamic-chunk-training과 --causal-convolution을 1로 설정하여 DCT 학습을 진행
+# --short-chunk-size는 일반적으로 0~25ms의 chunk size를 streaming 모델, 25ms~full context를 non streaming 모델이라고 함.
+# 25ms일 때, context를 가장 많이 참조하기 때문에 보통 25ms의 DCT를 사용
 # Streaming baseline : Conformer(L)-stateless-T
-./pruned_transducer_stateless5/train.py \
+./pruned_transducer_stateless4/train.py \
     --world-size 4 --num-epochs 30 --start-epoch 1 \
     --exp-dir pruned_transducer_stateless5/exp-960-streaming-baseline \
     --full-libri 1 --max-duration 350 --num-encoder-layers 17 \
@@ -18,28 +21,25 @@
     --dynamic-chunk-training 1 --causal-convolution 1 \
     --short-chunk-size 25 --num-left-chunks 4 \
     
-# 실험내용 : Time-retricted based chunk-wise
-# issue : encoder layer의 수에 따라 left chunk가 볼 수 있는 범위 계산 공식 필요
+# 실험내용 : Time-restricted based chunk-wise
+# training 옵션의 num-left-chunk는 streaming decoding 시 left-context와 조율해야함 : left-context = decode-chunk-size * num-left-chunks
+# acoustic feature의 stride가 30ms일 때, right-context를 한 frame만 적용한다면, num_layer * 30ms만큼의 latency가 발생
 # Streaming baseline : Conformer(L)-stateless-T
 ./pruned_transducer_stateless5/train.py \
     --world-size 4 --num-epochs 30 --start-epoch 1 \
-    --exp-dir pruned_transducer_stateless5/exp-960-streaming-left8 \
-    --full-libri 1 --max-duration 350 --num-encoder-layers 17 \
-    --dim-feedforward 2048 --nhead 8 --encoder-dim 512 \
-    --decoder-dim 512 --joiner-dim 640 \
-    --dynamic-chunk-training 1 --causal-convolution 1 \
-    --short-chunk-size 25 --num-left-chunks 8 \
-    
-
-# 실험내용 : Delay penalization
-# issue : warmup 하이퍼파라미터에 따라 학습이 수렴이 안되는 문제 해결중
-# Streaming baseline : Conformer(L)-stateless-T
-./pruned_transducer_stateless5/train.py \
-    --world-size 4 --num-epochs 30 --start-epoch 1 \
-    --exp-dir pruned_transducer_stateless5/exp-960-streaming-delay01\
+    --exp-dir pruned_transducer_stateless5/exp-960-streaming-left4 \
     --full-libri 1 --max-duration 350 --num-encoder-layers 17 \
     --dim-feedforward 2048 --nhead 8 --encoder-dim 512 \
     --decoder-dim 512 --joiner-dim 640 \
     --dynamic-chunk-training 1 --causal-convolution 1 \
     --short-chunk-size 25 --num-left-chunks 4 \
-    --delay-penalty 0.1 \
+
+# 실험내용 : Delay penalization
+# baseline 검증을 위해 icefall과 같은 모델 사이즈상에서 측정을 위해 ./pruned_transducer_stateless4의 conformer 모델 사용 
+# Streaming baseline : Conformer(L)-stateless-T
+./pruned_transducer_stateless4/train.py \
+    --world-size 4 --num-epochs 30 --start-epoch 1 \
+    --exp-dir pruned_transducer_stateless4/exp-960-streaming-p01 \
+    --full-libri 1 --max-duration 350 --dynamic-chunk-training 1 \
+    --causal-convolution 1 --short-chunk-size 25 --num-left-chunks 8 \
+    --delay-penalty 0.01
--- egs/librispeech/ASR/scripts/run_decode.sh
+++ egs/librispeech/ASR/scripts/run_decode.sh
@@ -9,6 +9,7 @@
 
 
 # streaming models decoding script
+# --decode-chunk-size : decoding 때 frame의 chunk size ex) 32 -> 320ms
 ./pruned_transducer_stateless5/decode.py \
     --epoch 25 --avg 10 \
     --exp-dir ./pruned_transducer_stateless5/exp-960-streaming \
@@ -16,3 +17,36 @@
     --num-encoder-layers 17 --dim-feedforward 2048 \
     --nhead 8 --encoder-dim 512 --decoder-dim 512 --joiner-dim 640 \
     --decode-chunk-len 32 \
+
+# greedy_search
+./pruned_transducer_stateless5/streaming_decode.py \
+    --epoch 30 --avg 10 \
+    --left-context 64 --decode-chunk-size 16 --right-context 0 \
+    --exp-dir ./pruned_transducer_stateless5/exp-960-streaming-left4 \
+    --num-encoder-layers 17 --dim-feedforward 2048 \
+    --nhead 8 --encoder-dim 512 --decoder-dim 512 --joiner-dim 640 \
+    --decoding-method greedy_search --num-decode-streams 200
+
+# fast_beam_search
+./pruned_transducer_stateless5/streaming_decode.py \
+    --epoch 30 --avg 10 \
+    --left-context 64 --decode-chunk-size 16 --right-context 0 \
+    --exp-dir ./pruned_transducer_stateless5/exp-960-streaming-left4 \
+    --num-encoder-layers 17 --dim-feedforward 2048 \
+    --nhead 8 --encoder-dim 512 --decoder-dim 512 --joiner-dim 640 \
+    --decoding-method fast_beam_search --num-decode-streams 200
+
+# modified_beam_search
+./pruned_transducer_stateless5/streaming_decode.py \
+    --epoch 30 --avg 10 \
+    --left-context 64 --decode-chunk-size 16 --right-context 0 \
+    --exp-dir ./pruned_transducer_stateless5/exp-960-streaming-left4 \
+    --num-encoder-layers 17 --dim-feedforward 2048 \
+    --nhead 8 --encoder-dim 512 --decoder-dim 512 --joiner-dim 640 \
+    --decoding-method modified_beam_search --num-decode-streams 200
+
+./pruned_transducer_stateless4/streaming_decode.py \
+    --epoch 30 --avg 10 \
+    --left-context 64 --decode-chunk-size 16 --right-context 0 \
+    --exp-dir ./pruned_transducer_stateless4/exp-960-streaming-p01 \
+    --decoding-method greedy_search --num-decode-streams 200
